5dbb74c1e6c5ca1347b430b6a2a4be3b91674073
Zyttal <calz483k@gmail.com>
2023-10-28 01:39:03 +0800

    Added Page Routing

--- lib/pages/infoPage.dart
+++ lib/pages/infoPage.dart
@@ -9,28 +9,50 @@ class InfoPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Stack(
-            clipBehavior: Clip.none,
-            children: [
-              Image.network(
-                  'https://res.cloudinary.com/dbwwffypj/image/upload/v1698290595/News_Application_UI_Assets/Rectangle_nwqvpg.png'),
-              Positioned(
-                top: 350,
-                child: InfoPageDetails(),
-              ),
-              Positioned(
-                  top: 315,
-                  left: 153,
-                  child: Image.network(
-                      'https://res.cloudinary.com/dbwwffypj/image/upload/v1698427170/News_Application_UI_Assets/Group_kmrznv.png')),
-              Positioned(
-                  left: 30,
-                  top: 30,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
+        child: GestureDetector(
+          onHorizontalDragEnd: (details) {
+            if (details.primaryVelocity! > 0) {
+              Navigator.of(context).pop(); // Swipe right, navigate back
+            }
+          },
+          child: SingleChildScrollView(
+            child: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                Image.network(
+                    'https://res.cloudinary.com/dbwwffypj/image/upload/v1698290595/News_Application_UI_Assets/Rectangle_nwqvpg.png'),
+                Positioned(
+                  top: 350,
+                  child: InfoPageDetails(),
+                ),
+                Positioned(
+                    top: 315,
+                    left: 153,
+                    child: Image.network(
+                        'https://res.cloudinary.com/dbwwffypj/image/upload/v1698427170/News_Application_UI_Assets/Group_kmrznv.png')),
+                Positioned(
+                    left: 30,
+                    top: 30,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.white,
+                          size: 20,
+                        ),
+                      ),
+                    )),
+                Positioned(
+                    top: 30,
+                    right: 30,
                     child: Container(
                       height: 50,
                       width: 50,
@@ -38,28 +60,13 @@ class InfoPage extends StatelessWidget {
                           border: Border.all(color: Colors.white),
                           borderRadius: BorderRadius.circular(20)),
                       child: Icon(
-                        Icons.arrow_back_ios_new,
+                        Icons.bookmark,
                         color: Colors.white,
                         size: 20,
                       ),
-                    ),
-                  )),
-              Positioned(
-                  top: 30,
-                  right: 30,
-                  child: Container(
-                    height: 50,
-                    width: 50,
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.white),
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Icon(
-                      Icons.bookmark,
-                      color: Colors.white,
-                      size: 20,
-                    ),
-                  ))
-            ],
+                    ))
+              ],
+            ),
           ),
         ),
       ),
